2241189d59aa955c88c0acc313d2bfa981882025
Sunin (Michael) Baek <144373604+suninbaek@users.noreply.github.com>
2025-02-17 12:13:32 +0900

    Delete js/load2.js

--- js/load2.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $("#header").load("../header.html",function(){
-        $.getScript("../js/nav.js");
-        $.getScript("../js/scroll.js");
-    });
-    $("#footer").load("../footer.html");
-});
